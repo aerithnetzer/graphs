adf4d5cb56bc03230aa8ce8f37670b2be452db5a
Aerith Netzer <aerithnetzer@outlook.com>
2025-11-03 16:48:18 -0600

    Add graph toy with diameter calculation

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <igraph/igraph.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  igraph_int_t num_vertices = 10000;
+  igraph_int_t num_edges = 100000;
+  igraph_real_t diameter, mean_degree;
+  igraph_t graph;
+
+  igraph_setup();
+
+  igraph_rng_seed(igraph_rng_default(), 42);
+
+  igraph_erdos_renyi_game_gnm(&graph, num_vertices, num_edges,
+                              IGRAPH_UNDIRECTED, IGRAPH_SIMPLE_SW,
+                              IGRAPH_EDGE_UNLABELED);
+  igraph_diameter(&graph, /* weights = */ NULL, &diameter,
+                  /* from = */ NULL, /* to = */ NULL,
+                  /* vertex_path = */ NULL, /* edge_path = */ NULL,
+                  IGRAPH_UNDIRECTED, /* unconn= */ true);
+
+  igraph_mean_degree(&graph, &mean_degree, IGRAPH_LOOPS);
+  printf("Diameter of a random graph with average degree %g: %g\n", mean_degree,
+         diameter);
+  igraph_destroy(&graph);
+  return 0;
+}
